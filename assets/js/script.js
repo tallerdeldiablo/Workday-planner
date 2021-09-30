e84556458dc19d24020840d53e1cb4f82274f022
Ricoding <57916204+tallerdeldiablo@users.noreply.github.com>
2021-09-30 15:26:59 -0400

    added comments
    and fixed a problem with the firs id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,23 @@
-var dayHour = moment().format("h a");
-var onlyhour = moment().format("H");
-//var lahora = moment().format("H"); // comparing the id with 24 hour to chance the class
+var onlyhour = moment().format("H"); // comparing the id with 24 hour to chance the class
+var days = moment().endOf('day').fromNow();
 var days = moment().endOf('day').fromNow();
 
 setInterval (function (){
-
-var reformatDate = moment().format("dddd, MMMM Do YYYY, h: a");
+var reformatDate = moment().format("dddd, MMMM Do YYYY, h:hh a");
     $("#currentDay").text(reformatDate);
 },1000)
 
-var toCheck= $("form-control")
-var container = $("#container");
-console.log(toCheck);
-
-var mainEL = $("main");
 //change the class
-$('.form-control').each(function() {
-  console.log($(this).text());
-   var compa = ($(this).attr('id'))
-   console.log(compa);
-    var formi = $('.form-control')
+$('.form-control').each(function() { //compare each id that is iqual to each hour 
 
-    if ( compa < onlyhour) {
-      $(this).addClass("form-control  bg-secondary")
-    }else if  (compa ==  onlyhour)    {
-      $(this).addClass("form-control  bg-danger")
-    }
-    else
-    $(this).addClass("form-control  bg-success")
+   var compa = ($(this).attr('id'))  // get the id
+  compa *= 1;  // it fixed the problem with the first id
 
+     if ( compa < onlyhour) {                                  //this is past
+       $(this).addClass("form-control  bg-secondary")          // add color grey
+     }else if  (compa ==  onlyhour)    {                        //this is the same time
+          $(this).addClass("form-control  bg-danger")           
+       }
+       else
+           $(this).addClass("form-control  bg-success")
    });
